7b1ce21c18c9a2a08219c59371883bf9a5fc2ebd
Sree Sai Raghava <raghava.rym@gmail.com>
2021-08-05 06:12:52 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Sree Sai Raghava Dandu on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
